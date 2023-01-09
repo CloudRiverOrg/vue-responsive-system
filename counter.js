f8f9b4bf611ff8782852165360d80e58f619603e
jingxuan <lizhigang@51kuafu.com>
2023-01-10 01:41:08 +0800

    V3.1 依赖存放的数据结构修整

--- counter.js
+++ counter.js
@@ -1,7 +1,9 @@
 let obj = { counter: 0 }
 let btn = ''
 
-const effectFnSet = new Set()
+// WeakMap 相比 map： 
+//  obj被回收的时候，WeakMap不会占用内存，map会拿着引用一直无法释放，需要手动删除，否则内存溢出风险
+const effectBigMap = new WeakMap()
 
 let currentEffect 
 function registerCurrentEffect(fn) {
@@ -12,13 +14,28 @@ const effect = registerCurrentEffect
 
 const objRes = new Proxy(obj, {
   get(target, key) {
-    // 现在收集依赖固定函数名的耦合解除了 
-    currentEffect && effectFnSet.add(currentEffect)
+    if (!currentEffect) return target[key]
+    // 每个依赖对象obj 都是WeakMap的一个key， 值为 一个depsMap(具体依赖map)，数据结构如 { counter: effectSet[] }
+    let depsMap = effectBigMap.get(target)
+    if (!depsMap) {
+      depsMap = new Map()
+      effectBigMap.set(target, depsMap)
+      // effectBigMap.set(target, (depsMap = new Map()))
+    }
+
+    let deps = depsMap.get(key) // effectSet
+    if (!deps) {
+      depsMap.set(key,deps = new Set())
+    }
+
+    deps.add(currentEffect)
+    console.log('add over', depsMap);
     return target[key]
   },
   set(target, key, newVal) {
     target[key] = newVal
-    effectFnSet.forEach(fn => fn())
+    let deps = effectBigMap.get(target)?.get(key)
+    deps && deps.forEach(fn => fn())
     return true
   }
 })
@@ -30,8 +47,8 @@ export function setupCounter() {
 }
 
 export function setBtnText() {
-  console.log('DOM: setBtnText');
   btn.innerHTML = `count is ${objRes.counter}`
+  console.log('DOM: setBtnText', effectBigMap);
 }
 
 setTimeout(() => {
@@ -41,3 +58,19 @@ setTimeout(() => {
   objRes.c = 1
   objRes.d = 1
 })
+
+/*
+effectBigMap 最后的数据结构示例
+
+effectBigMap
+  |--userInfo
+      |--userName
+          |-- effect1(SetItemFn)
+          |-- effect2
+
+  |--obj2
+      |--key1
+          |-- effect1
+      |--key1
+          |-- effect2
+*/
