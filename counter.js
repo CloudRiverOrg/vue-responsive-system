88ee59ed1cb5c4a769eafe05833451ac41a291e2
jingxuan <lizhigang@51kuafu.com>
2023-01-10 00:46:33 +0800

    V2

--- counter.js
+++ counter.js
@@ -1,15 +1,26 @@
 let obj = { counter: 0 }
 let btn = ''
 
+const effectFnSet = new Set()
+
+const objRes = new Proxy(obj, {
+  get(target, key) {
+    effectFnSet.add(setBtnText)
+    return target[key]
+  },
+  set(target, key, newVal) {
+    effectFnSet.forEach(fn => fn())
+    target[key] = newVal
+    return true
+  }
+})
+
 export function setupCounter() {
   btn = document.querySelector('#counter')
-  btn.addEventListener('click', () => obj.counter++)
+  btn.addEventListener('click', () => objRes.counter++)
 }
 
 export function setBtnText() {
-  btn.innerHTML = `count is ${obj.counter}`
+  btn.innerHTML = `count is ${objRes.counter}`
 }
 
-function effect () {
-  // 结果 按钮点击后 触发 obj.counter++, 按钮内的内容文本自动更新
-}
